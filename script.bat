eab7377eac41395fc498a241da44e1c2483e67e4
Prudhvi Reddy N <prudhvireddy099@gmail.com>
2020-04-16 11:48:44 +0530

    Update script.bat

--- script.bat
+++ script.bat
@@ -1,1 +1,8 @@
+@ECHO OFF
+ECHO Written by: Prudhvi Reddy Nallandhula
+ECHO.
+
+echo msgbox "Click OK to open Timesheet" ,4160, "Timesheet" > %tmp%\tmp.vbs
+wscript %tmp%\tmp.vbs
 cmd /c start https://www.google.com
+del %tmp%\tmp.vbs
