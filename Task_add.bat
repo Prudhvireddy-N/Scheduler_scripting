feda334ee65a1e0b2cd6b4c270933f8cf6b3cb0f
Prudhvi Reddy N <prudhvireddy099@gmail.com>
2020-04-16 11:45:42 +0530

    Update Task_add.bat

--- Task_add.bat
+++ Task_add.bat
@@ -1,4 +1,9 @@
+@ECHO OFF
+ECHO Written by: Prudhvi Reddy Nallandhula
+ECHO.
+
 set mypath=%cd%
 @echo %mypath%
+copy %mypath%\script.vbs C:\Users\%USERNAME%
 copy %mypath%\script.bat C:\Users\%USERNAME%
-SchTasks /Create /SC DAILY /TN custom_task /TR C:\Users\%USERNAME%\script.bat /ST 09:00
+SchTasks /Create /SC WEEKLY /D MON,TUE,WED,THU,FRI /TN custom_task /TR C:\Users\%USERNAME%\script.vbs /ST 16:30
